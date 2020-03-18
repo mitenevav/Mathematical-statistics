7bcbba33eb11255135c4ffd580ac991bcb11a70e
Aleksandr Mitenev <aleksandr.mitenev@mail.ru>
2020-03-18 08:48:14 +0300

    lab_2

--- lab_1.py
+++ lab_1.py
@@ -3,81 +3,483 @@ from scipy.stats import *
 from numpy import random
 import matplotlib.pyplot as plt
 
-buf_size =[10,50,1000]
-size_columns=[10,15,20]
+buf_size = [10,100,1000]
 
 
-c_lim=[2.5,3,4]
+
 # NORMAL
 for i in range(len(buf_size)):
-    plt.subplot(1,len(buf_size), i+1)
-    x = norm.rvs(scale = 1,size=buf_size[i])
-    plt.hist(x, size_columns[i], density=True, histtype='stepfilled', alpha=0.5)
-    y = linspace(-c_lim[i], c_lim[i], 1000)
-    plt.plot(y,norm.pdf(y))
-    plt.xlabel("n="+ str(buf_size[i]))
-    plt.ylabel("Плотность распределения")
-plt.title("Нормальное распределение")
-plt.show()
+    sum1 = 0
+    sum2 = 0
+    sum3 = 0
+    sum4 = 0
+    sum5 = 0
+
+    sum21 = 0
+    sum22 = 0
+    sum23 = 0
+    sum24 = 0
+    sum25 = 0
 
 
+    print()
+    print("NORMAL", end=" ")
+    print(buf_size[i])
+
+    for k in range(1000):
+        x1 = 0
+        med = 0
+        zR = 0
+        zp1 = 0
+        zp3 = 0
+        zQ = 0
+        ztr = 0
+
+
+        x = norm.rvs(scale = 1,size=buf_size[i])
+
+        for j in range(buf_size[i]):
+            x1 += x[j]
+        x1 /= buf_size[i]
+
+        sum1 += x1
+        sum21 += x1*x1
+
+        med = (x[int(buf_size[i] / 2 - 1)] + x[int(buf_size[i] / 2)]) / 2
+        sum2 += med
+        sum22 += med*med
+
+        zR = (x[0] + x[buf_size[i]-1])/2
+        sum3 += zR
+        sum23 += zR*zR
+
+
+        if i==0:
+            zp1 = x[int(buf_size[i] / 4) + 1]
+            zp3 = x[int(buf_size[i] * 3 / 4) + 1]
+        else:
+            zp1 = x[int(buf_size[i] / 4)]
+            zp3 = x[int(buf_size[i] * 3 / 4)]
+        zQ = (zp1 + zp3) / 2
+        sum4 += zQ
+        sum24 += zQ*zQ
+
+
+        r = int(buf_size[i] / 4)
+        for j in range(r+1, buf_size[i] - r):
+            ztr += x[j]
+        ztr /= (buf_size[i] - 2*r)
+        sum5 += ztr
+        sum25 += ztr*ztr
+
+    sum1 /= 1000
+    sum2 /= 1000
+    sum3 /= 1000
+    sum4 /= 1000
+    sum5 /= 1000
+
+    sum21 /= 1000
+    sum22 /= 1000
+    sum23 /= 1000
+    sum24 /= 1000
+    sum25 /= 1000
+
+    sum21 -= sum1*sum1
+    sum22 -= sum2*sum2
+    sum23 -= sum3*sum3
+    sum24 -= sum4*sum4
+    sum25 -= sum5*sum5
+
+    print("x1 =", sum1)
+    print("med =", sum2)
+    print("zR =", sum3)
+    print("zQ =", sum4)
+    print("ztr =", sum5)
+    print()
+    print("D x1 =", sum21)
+    print("D med =", sum22)
+    print("D zR =", sum23)
+    print("D zQ =", sum24)
+    print("D ztr =", sum25)
+
+print()
+print("---------------------------------------LAPLACE----------------------------------------------------------")
 #   LAPLACE
-plt.figure()
-c_lim=[3,5,10]
 for i in range(len(buf_size)):
-    plt.subplot(1,len(buf_size), i+1)
-    x =  laplace.rvs(scale = sqrt(1/2),size=buf_size[i])
-    plt.hist(x, size_columns[i], density=True, histtype='stepfilled', alpha=0.5)
-    y = linspace(-c_lim[i], c_lim[i], 1000)
-    plt.plot(y,laplace.pdf(y))
-    plt.xlabel("n="+ str(buf_size[i]))
-    plt.ylabel("Плотность распределения")
-plt.title("Распределение Лапласа")
-plt.show()
+    sum1 = 0
+    sum2 = 0
+    sum3 = 0
+    sum4 = 0
+    sum5 = 0
 
 
-c_col=[15, 30, 500]
-#c_lim=[10,20,50]
+    sum21 = 0
+    sum22 = 0
+    sum23 = 0
+    sum24 = 0
+    sum25 = 0
+
+
+    print()
+    print("LAPLACE", end=" ")
+    print(buf_size[i])
+
+    for k in range(1000):
+        x1 = 0
+        med = 0
+        zR = 0
+        zp1 = 0
+        zp3 = 0
+        zQ = 0
+        ztr = 0
+
+        x =  laplace.rvs(scale = sqrt(1/2),size=buf_size[i])
+
+        for j in range(buf_size[i]):
+            x1 += x[j]
+        x1 /= buf_size[i]
+
+        sum1 += x1
+        sum21 += x1 * x1
+
+        med = (x[int(buf_size[i] / 2 - 1)] + x[int(buf_size[i] / 2)]) / 2
+        sum2 += med
+        sum22 += med * med
+
+        zR = (x[0] + x[buf_size[i] - 1]) / 2
+        sum3 += zR
+        sum23 += zR * zR
+
+        if i == 0:
+            zp1 = x[int(buf_size[i] / 4) + 1]
+            zp3 = x[int(buf_size[i] * 3 / 4) + 1]
+        else:
+            zp1 = x[int(buf_size[i] / 4)]
+            zp3 = x[int(buf_size[i] * 3 / 4)]
+        zQ = (zp1 + zp3) / 2
+        sum4 += zQ
+        sum24 += zQ * zQ
+
+        r = int(buf_size[i] / 4)
+        for j in range(r + 1, buf_size[i] - r):
+            ztr += x[j]
+        ztr /= (buf_size[i] - 2 * r)
+        sum5 += ztr
+        sum25 += ztr * ztr
+
+    sum1 /= 1000
+    sum2 /= 1000
+    sum3 /= 1000
+    sum4 /= 1000
+    sum5 /= 1000
+
+    sum21 /= 1000
+    sum22 /= 1000
+    sum23 /= 1000
+    sum24 /= 1000
+    sum25 /= 1000
+
+    sum21 -= sum1 * sum1
+    sum22 -= sum2 * sum2
+    sum23 -= sum3 * sum3
+    sum24 -= sum4 * sum4
+    sum25 -= sum5 * sum5
+
+    print("x1 =", sum1)
+    print("med =", sum2)
+    print("zR =", sum3)
+    print("zQ =", sum4)
+    print("ztr =", sum5)
+    print()
+    print("D x1 =", sum21)
+    print("D med =", sum22)
+    print("D zR =", sum23)
+    print("D zQ =", sum24)
+    print("D ztr =", sum25)
+print()
+print("---------------------------------------CAUCHY----------------------------------------------------------")
+
 #   CAUCHY
-plt.figure()
+
 for i in range(len(buf_size)):
-    plt.subplot(1,len(buf_size), i+1)
-    x = cauchy.rvs(size=buf_size[i])
-    plt.hist(x, c_col[i], density=True, histtype='stepfilled', alpha=0.5)
-    y = linspace(min(x)-3, max(x)+3, 1000)
-    plt.plot(y,cauchy.pdf(y))
-    plt.xlabel("n="+ str(buf_size[i]))
-    plt.ylabel("Плотность распределения")
-    plt.xlim(min(x)-3, max(x)+3)
-plt.title("Распределение Коши")
-plt.show()
+    sum1 = 0
+    sum2 = 0
+    sum3 = 0
+    sum4 = 0
+    sum5 = 0
+
+    sum21 = 0
+    sum22 = 0
+    sum23 = 0
+    sum24 = 0
+    sum25 = 0
+
+    print()
+    print("CAUCHY", end=" ")
+    print(buf_size[i])
+
+    for k in range(1000):
+        x1 = 0
+        med = 0
+        zR = 0
+        zp1 = 0
+        zp3 = 0
+        zQ = 0
+        ztr = 0
+
+
+        x = cauchy.rvs(size=buf_size[i])
+
+        for j in range(buf_size[i]):
+            x1 += x[j]
+        x1 /= buf_size[i]
+
+        sum1 += x1
+        sum21 += x1 * x1
+
+        med = (x[int(buf_size[i] / 2 - 1)] + x[int(buf_size[i] / 2)]) / 2
+        sum2 += med
+        sum22 += med * med
+
+        zR = (x[0] + x[buf_size[i] - 1]) / 2
+        sum3 += zR
+        sum23 += zR * zR
+
+        if i == 0:
+            zp1 = x[int(buf_size[i] / 4) + 1]
+            zp3 = x[int(buf_size[i] * 3 / 4) + 1]
+        else:
+            zp1 = x[int(buf_size[i] / 4)]
+            zp3 = x[int(buf_size[i] * 3 / 4)]
+        zQ = (zp1 + zp3) / 2
+        sum4 += zQ
+        sum24 += zQ * zQ
+
+        r = int(buf_size[i] / 4)
+        for j in range(r + 1, buf_size[i] - r):
+            ztr += x[j]
+        ztr /= (buf_size[i] - 2 * r)
+        sum5 += ztr
+        sum25 += ztr * ztr
+
+    sum1 /= 1000
+    sum2 /= 1000
+    sum3 /= 1000
+    sum4 /= 1000
+    sum5 /= 1000
+
+    sum21 /= 1000
+    sum22 /= 1000
+    sum23 /= 1000
+    sum24 /= 1000
+    sum25 /= 1000
+
+    sum21 -= sum1 * sum1
+    sum22 -= sum2 * sum2
+    sum23 -= sum3 * sum3
+    sum24 -= sum4 * sum4
+    sum25 -= sum5 * sum5
+
+    print("x1 =", sum1)
+    print("med =", sum2)
+    print("zR =", sum3)
+    print("zQ =", sum4)
+    print("ztr =", sum5)
+    print()
+    print("D x1 =", sum21)
+    print("D med =", sum22)
+    print("D zR =", sum23)
+    print("D zQ =", sum24)
+    print("D ztr =", sum25)
+print()
+print("---------------------------------------POISSION----------------------------------------------------------")
 
 #   POISSION
-plt.figure()
 
-c_lim=[20,20,30]
 for i in range(len(buf_size)):
-    plt.subplot(1,len(buf_size), i+1)
-    x = poisson.rvs(10, size=buf_size[i])
-    plt.hist(x, size_columns[i], density=True, histtype='stepfilled', alpha=0.5)
-    y=linspace(0,c_lim[i],c_lim[i]+1)
-    plt.plot(y,poisson.pmf(y, 10))
-    plt.xlabel("n="+ str(buf_size[i]))
-    plt.ylabel("Плотность распределения")
-plt.title("Распределение Пуассона")
-plt.show()
+
+    sum1 = 0
+    sum2 = 0
+    sum3 = 0
+    sum4 = 0
+    sum5 = 0
+
+    sum21 = 0
+    sum22 = 0
+    sum23 = 0
+    sum24 = 0
+    sum25 = 0
+
+    print()
+    print("POISSION", end=" ")
+    print(buf_size[i])
+
+    for k in range(1000):
+        x1 = 0
+        med = 0
+        zR = 0
+        zp1 = 0
+        zp3 = 0
+        zQ = 0
+        ztr = 0
+
+        x = poisson.rvs(10, size=buf_size[i])
+
+        for j in range(buf_size[i]):
+            x1 += x[j]
+        x1 /= buf_size[i]
+
+        sum1 += x1
+        sum21 += x1 * x1
+
+        med = (x[int(buf_size[i] / 2 - 1)] + x[int(buf_size[i] / 2)]) / 2
+        sum2 += med
+        sum22 += med * med
+
+        zR = (x[0] + x[buf_size[i] - 1]) / 2
+        sum3 += zR
+        sum23 += zR * zR
+
+        if i == 0:
+            zp1 = x[int(buf_size[i] / 4) + 1]
+            zp3 = x[int(buf_size[i] * 3 / 4) + 1]
+        else:
+            zp1 = x[int(buf_size[i] / 4)]
+            zp3 = x[int(buf_size[i] * 3 / 4)]
+        zQ = (zp1 + zp3) / 2
+        sum4 += zQ
+        sum24 += zQ * zQ
+
+        r = int(buf_size[i] / 4)
+        for j in range(r + 1, buf_size[i] - r):
+            ztr += x[j]
+        ztr /= (buf_size[i] - 2 * r)
+        sum5 += ztr
+        sum25 += ztr * ztr
+
+    sum1 /= 1000
+    sum2 /= 1000
+    sum3 /= 1000
+    sum4 /= 1000
+    sum5 /= 1000
+
+    sum21 /= 1000
+    sum22 /= 1000
+    sum23 /= 1000
+    sum24 /= 1000
+    sum25 /= 1000
+
+    sum21 -= sum1 * sum1
+    sum22 -= sum2 * sum2
+    sum23 -= sum3 * sum3
+    sum24 -= sum4 * sum4
+    sum25 -= sum5 * sum5
+
+    print("x1 =", sum1)
+    print("med =", sum2)
+    print("zR =", sum3)
+    print("zQ =", sum4)
+    print("ztr =", sum5)
+    print()
+    print("D x1 =", sum21)
+    print("D med =", sum22)
+    print("D zR =", sum23)
+    print("D zQ =", sum24)
+    print("D ztr =", sum25)
+
+
+print()
+print("---------------------------------------UNIFORM----------------------------------------------------------")
 
 
 #   UNIFORM
-plt.figure()
 for i in range(len(buf_size)):
-    plt.subplot(1,len(buf_size), i+1)
-    x = random.uniform(-sqrt(3), sqrt(3), buf_size[i])
-    plt.hist(x, size_columns[i], density=True, histtype='stepfilled', alpha=0.5)
-    y = linspace(-sqrt(3), sqrt(3), 2)
-    yy = [1/2/sqrt(3), 1/2/sqrt(3)]
-    plot(y, yy)
-    plt.xlabel("n="+ str(buf_size[i]))
-    plt.ylabel("Плотность распределения")
-plt.title("Равномерное распределение")
-plt.show()
+    sum1 = 0
+    sum2 = 0
+    sum3 = 0
+    sum4 = 0
+    sum5 = 0
+
+    sum21 = 0
+    sum22 = 0
+    sum23 = 0
+    sum24 = 0
+    sum25 = 0
+
+    print()
+    print("UNIFORM", end=" ")
+    print(buf_size[i])
+
+    for k in range(1000):
+        x1 = 0
+        med = 0
+        zR = 0
+        zp1 = 0
+        zp3 = 0
+        zQ = 0
+        ztr = 0
+
+        x = random.uniform(-sqrt(3), sqrt(3), buf_size[i])
+
+        for j in range(buf_size[i]):
+            x1 += x[j]
+        x1 /= buf_size[i]
+
+        sum1 += x1
+        sum21 += x1 * x1
+
+        med = (x[int(buf_size[i] / 2 - 1)] + x[int(buf_size[i] / 2)]) / 2
+        sum2 += med
+        sum22 += med * med
+
+        zR = (x[0] + x[buf_size[i] - 1]) / 2
+        sum3 += zR
+        sum23 += zR * zR
+
+        if i == 0:
+            zp1 = x[int(buf_size[i] / 4) + 1]
+            zp3 = x[int(buf_size[i] * 3 / 4) + 1]
+        else:
+            zp1 = x[int(buf_size[i] / 4)]
+            zp3 = x[int(buf_size[i] * 3 / 4)]
+        zQ = (zp1 + zp3) / 2
+        sum4 += zQ
+        sum24 += zQ * zQ
+
+        r = int(buf_size[i] / 4)
+        for j in range(r + 1, buf_size[i] - r):
+            ztr += x[j]
+        ztr /= (buf_size[i] - 2 * r)
+        sum5 += ztr
+        sum25 += ztr * ztr
+
+    sum1 /= 1000
+    sum2 /= 1000
+    sum3 /= 1000
+    sum4 /= 1000
+    sum5 /= 1000
+
+    sum21 /= 1000
+    sum22 /= 1000
+    sum23 /= 1000
+    sum24 /= 1000
+    sum25 /= 1000
+
+    sum21 -= sum1 * sum1
+    sum22 -= sum2 * sum2
+    sum23 -= sum3 * sum3
+    sum24 -= sum4 * sum4
+    sum25 -= sum5 * sum5
+
+    print("x1 =", sum1)
+    print("med =", sum2)
+    print("zR =", sum3)
+    print("zQ =", sum4)
+    print("ztr =", sum5)
+    print()
+    print("D x1 =", sum21)
+    print("D med =", sum22)
+    print("D zR =", sum23)
+    print("D zQ =", sum24)
+    print("D ztr =", sum25)
+
